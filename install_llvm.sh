e8223c7e0061cd792b709193b09cc05a343d6dc5
Anjia Wang <anjiawang@gmail.com>
2020-08-16 15:11:41 -0400

    Update the scripts for LLVM installation and setup

---
+++ install_llvm.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    LLVM=$HOME/llvm
+else
+    LLVM=$1
+fi
+
+if [ -z "$2" ]; then
+    LLVM_VERSION=10
+else
+    LLVM_VERSION=$2
+fi
+
+if [ -z "$3" ]; then
+    USE_GPU=0
+else
+    USE_GPU=1
+fi
+
+export LLVM_SRC=$LLVM/llvm_src
+export LLVM_PATH=$LLVM/llvm_install
+export LLVM_BUILD=$LLVM/llvm_build
+
+mkdir -p $LLVM_SRC
+mkdir -p $LLVM_PATH
+mkdir -p $LLVM_BUILD
+cd $LLVM_SRC
+git clone -b release/$LLVM_VERSION.x https://github.com/llvm/llvm-project .
+cd $LLVM_BUILD
+if [$USE_GPU == 0]; then
+    cmake -G Ninja  -DLLVM_USE_LINKER=gold  -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$LLVM_PATH -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;openmp;libcxx;libcxxabi" $LLVM_SRC/llvm
+else
+    cmake -G Ninja  -DLLVM_USE_LINKER=gold  -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=$LLVM_PATH -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;openmp;libcxx;libcxxabi" -DLLVM_TARGETS_TO_BUILD="host;NVPTX" -DLLVM_BUILD_EXAMPLES=ON $LLVM_SRC/llvm
+fi
+
+ninja -j6 -l6
+ninja install -j6
+
+export PATH=$LLVM_PATH/bin:$PATH
+export LD_LIBRARY_PATH=$LLVM_PATH/libexec:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LLVM_PATH/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$LLVM_PATH/libexec:$LIBRARY_PATH
+export LIBRARY_PATH=$LLVM_PATH/lib:$LIBRARY_PATH
+export MANPATH=$LLVM_PATH/share/man:$MANPATH
+export C_INCLUDE_PATH=$LLVM_PATH/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$LLVM_PATH/include:CPLUS_INCLUDE_PATH
+
--- setup_llvm.sh
+++ setup_llvm.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Please only run this script in the root folder of ROSE/REX compiler,
-# which has the compiler source code as subfolder.
+if [ -z "$1" ]; then
+    echo "Please provide a location of LLVM installation."
+    return -1 2>/dev/null
+    exit -1
+else
+    cd $1
+    LLVM=$(pwd)
+    cd -
+fi
 
-export LLVM_SRC=$HOME/Projects/llvm9/llvm_src
-export LLVM_PATH=$HOME/Projects/llvm9/llvm_install
-export LLVM_BUILD=$HOME/Projects/llvm9/llvm_build
-
-mkdir -p $LLVM_SRC
-mkdir -p $LLVM_PATH
-mkdir -p $LLVM_BUILD
-cd $LLVM_SRC
-git clone -b release/9.x https://github.com/llvm/llvm-project .
-cd $LLVM_BUILD
-cmake -G Ninja  -DLLVM_USE_LINKER=gold  -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$LLVM_PATH -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;openmp;libcxx;libcxxabi" $LLVM_SRC/llvm
-ninja install -j6 -l6
+export LLVM_SRC=$LLVM/llvm_src
+export LLVM_PATH=$LLVM/llvm_install
+export LLVM_BUILD=$LLVM/llvm_build
 
 export PATH=$LLVM_PATH/bin:$PATH
 export LD_LIBRARY_PATH=$LLVM_PATH/libexec:$LD_LIBRARY_PATH
@@ -24,4 +22,3 @@ export LIBRARY_PATH=$LLVM_PATH/lib:$LIBRARY_PATH
 export MANPATH=$LLVM_PATH/share/man:$MANPATH
 export C_INCLUDE_PATH=$LLVM_PATH/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=$LLVM_PATH/include:CPLUS_INCLUDE_PATH
-
