ee3dd8e65ce52b211ace5ed573e2f72b5a03961e
Anjia Wang <anjiawang@gmail.com>
2020-09-03 20:45:13 -0400

    Clean up code and remove CI

--- llvm7/setup_omp.sh
+++
@@ -1,6 +0,0 @@
-
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(pwd)/../install \
-	-DCMAKE_C_COMPILER=$(pwd)/../install/bin/clang \
-	-DCMAKE_CXX_COMPILER=$(pwd)/../install/bin/clang++ \
-	-DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=35,60,70 \
-	../llvm-7.1.0.src/projects/openmp
