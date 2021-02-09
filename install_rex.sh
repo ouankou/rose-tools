902765228da87b54d50caff2849492162bf2d17e
Anjia Wang <anjiawang@gmail.com>
2021-02-09 03:11:07 -0500

    Add a script to build REX from scratch

---
+++ install_rex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REX_ROOT=$HOME/Projects/rexdex
+mkdir -p $REX_ROOT
+cd $REX_ROOT
+git clone -b dev git@github.com:passlab/rexompiler.git rex_src
+mkdir rex_build
+cd rex_src
+git submodule update --init
+./build
+cd ../rex_build
+../rex_src/configure --prefix=$REX_ROOT/rex_install --with-boost=/usr --with-boost-libdir=/usr/lib/x86_64-linux-gnu/ --enable-languages=c,c++,fortran --disable-tests-directory --disable-tutorial-directory
+make core -j6
+make install-core
